449dc3a67c19cdf135c9c0f1014ce49e84c7b7d7
compensations <112657862+compensations@users.noreply.github.com>
2022-09-03 14:07:38 -0400

    Create bypassroblox.bat

---
+++ bypassroblox.bat
@@ -0,0 +1,60 @@
+@echo off
+echo **This file must be ran with administrator privileges**
+echo ewo#9438
+echo 128.116.119.4 www.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 web.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 abtesting.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 accountinformation.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 accountsettings.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 adconfiguration.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 ads.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 api.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 assetdelivery.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 auth.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 avatar.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 badges.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 billing.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 catalog.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 cdnproviders.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 chat.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 clientsettings.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 contentstore.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 contacts.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 develop.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 discussions.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 economy.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 economycreatorstats.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 engagementpayouts.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 followings.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 friends.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 gameinternationalization.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 gamejoin.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 gamepersistence.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 games.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 groups.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 groupsmoderation.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 inventory.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 itemconfiguration.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 locale.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 localizationtables.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 metrics.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 midas.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 notifications.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 points.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 premiumfeatures.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 presence.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 privatemessages.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 publish.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 share.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 textfilter.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 thumbnails.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 thumbnailsresizer.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 trades.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 translationroles.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 translations.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 twostepverification.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 users.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+echo 128.116.119.4 voice.roblox.com >> %HOMEDRIVE%/Windows/System32/drivers/etc/hosts
+ipconfig /flushdns
+echo Done!
+pause
